68c5c45a68f3d4791ace317f0a26eae56f46e6fe
Tom Molesworth <cpan@perlsite.co.uk>
2014-08-04 17:07:54 +0100

    Missing Roster class

---
+++ lib/Protocol/XMPP/Roster.pm
@@ -0,0 +1,18 @@
+package Protocol::XMPP::Roster;
+use strict;
+use warnings;
+
+sub new { my $class = shift; bless { @_ }, $class; }
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Tom Molesworth <cpan@entitymodel.com>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2010-2014. Licensed under the same terms as Perl itself.
+
